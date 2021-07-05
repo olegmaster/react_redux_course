98e3bd27bcf6a1b6c36d5679e440234f54e19f96
Oleg <o.prystashkyn@program-ace.com.ua>
2021-07-05 18:59:49 +0300

    add click listeners on add and remove todo

--- src/todos/Todolist.js
+++ src/todos/Todolist.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import NewTodoForm from "./NewTodoForm";
 import TodolistItem from './TodolistItem';
+import { removeTodo } from "./actions";
+import { connect } from "react-redux";
 import "./Todolist.css";
 
-const Todolist = ({todos = [{text: "Hello Oleg"}]}) => (
+const Todolist = ({todos = [{text: "Hello Oleg"}], onRemovePressed}) => (
     <div className="list-wrapper">
         <NewTodoForm />
-        {todos.map(todo => <TodolistItem todo={todo} />)}
+        {todos.map(todo => <TodolistItem todo={todo} onRemovePressed={onRemovePressed} />)}
     </div>
 );
 
-export default Todolist;
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+    onRemovePressed: text => dispatch(removeTodo(text)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todolist);
--- src/todos/TodolistItem.js
+++ src/todos/TodolistItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './TodolistItem.css'
 
-const TodolistItem = ({ todo }) => (
+const TodolistItem = ({ todo, onRemovePressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="buttons-container">
             <button className="completed-button">Mark As this is As</button>
-            <button className="remove-button">Delete</button>
+            <button
+                className="remove-button"
+                onClick={ () => onRemovePressed(todo.text)}
+            >Delete</button>
         </div>
     </div>
 );
